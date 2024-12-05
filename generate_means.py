839a944499c5cb0b526ea1238df59d4d13bad7c8
iwonieevo <280043@student.pwr.edu.pl>
2024-12-05 01:38:52 +0100

    reformatting some code

--- generate_means.py
+++ generate_means.py
@@ -9,21 +9,22 @@ def generate_random_coordinates(h, w, n):
     x_random = _rand % w
     return y_random, x_random
 
-if __name__ == '__main__':
-    DIR = r"data"
-    OUTPUT_DIR = r"json_data"
-    if not os.path.exists(OUTPUT_DIR):
-        os.makedirs(OUTPUT_DIR)
-    num_samples = 100
-    num_means = 100
-    height, width = 100, 100
-    coordinates_list = [generate_random_coordinates(height, width, num_samples) for i in range(num_means)]
-    for name in os.listdir(DIR):
-        image = cv2.cvtColor(cv2.imread(os.path.join(DIR, name)), cv2.COLOR_BGR2GRAY)
+def generate_means(data_dir, output_dir, num_samples, num_means, dims):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    coordinates_list = [generate_random_coordinates(*dims, num_samples) for _ in range(num_means)]
+    for name in os.listdir(data_dir):
+        image = cv2.cvtColor(cv2.imread(os.path.join(data_dir, name)), cv2.COLOR_BGR2GRAY)
         means = {}
+
         i = 1
         for coordinates in coordinates_list:
             means[f"F_{i}"] = np.mean(image[coordinates])
             i += 1
-        with open(os.path.join(OUTPUT_DIR, f"{name[:-4]}.json"), "w") as f:
+
+        with open(os.path.join(output_dir, f"{name[:-4]}.json"), "w") as f:
             f.write(json.dumps(means, indent=4))
+
+if __name__ == '__main__':
+    generate_means(data_dir=r"data", output_dir=r"json_data", num_samples=100, num_means=100, dims=(100, 100))
