1e5d0186b70f4c4345599290d19fdd887cd48f62
Barbecki <280075@student.pwr.edu.pl>
2025-01-15 04:03:33 +0100

    HOG, magnitude spectrum, wiecej ROC

--- main.py
+++ main.py
@@ -2,151 +2,110 @@ import os
 import numpy as np
 import cv2
 import csv
-from sklearn.metrics import roc_curve, roc_auc_score
 import matplotlib.pyplot as plt
-import scipy.stats as stats
+from skimage.metrics import structural_similarity as ssim
+from skimage.feature import hog
+from skimage import exposure
 
-def generate_random_coordinates(h: int, w: int, n: int) -> tuple:
-    """
-    Generating tuple of n random coordinates
-    :param h: height of the matrix
-    :param w: width of the matrix
-    :param n: number of random coordinates to generate
-    :return: (ndarray, ndarray); generated coordinates (row index, column index)
-    """
-    _rand = np.random.default_rng().choice(w * h, size=n, replace=False)
-    y_random = _rand // w
-    x_random = _rand % w
-    return y_random, x_random
 
-def generate_means(data_dir, coordinates_list) -> None:
+def save_magnitude_spectrum(image_path, output_path):
     """
-    Generating means of values from data from coordinates and saving them in json file
-    :param data_dir: path to directory with data (grayscale images with .png extension)
-    :param coordinates_list: list of lists of coordinates to calculate means from
+    Save the magnitude spectrum of a single image to a file
+    :param image_path: path to the image file
+    :param output_path: path to save the magnitude spectrum image
     :return: None
     """
-    with open('data.csv', 'w', newline='') as f:
-        for name in os.listdir(data_dir):
-            if name.endswith('.gif'):
-                ret, image = cv2.VideoCapture(os.path.join(data_dir, name)).read()
-                if not ret:
-                    raise AssertionError("Error with loading image.")
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, image = cv2.VideoCapture(image_path).read()
+    if not ret:
+        raise AssertionError("Error with loading image.")
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-                means = []
-                for coordinates in coordinates_list:
-                    means.append(float(np.mean(image[coordinates])))
-                csv.writer(f, delimiter=';').writerow(means)
+    # Apply Fourier Transform
+    f_transform = np.fft.fft2(image)
+    f_transform_shifted = np.fft.fftshift(f_transform)
+    magnitude_spectrum = 20 * np.log(np.abs(f_transform_shifted))
+    
+    # Normalize the magnitude spectrum to the range [0, 255]
+    magnitude_spectrum = cv2.normalize(magnitude_spectrum, None, 0, 255, cv2.NORM_MINMAX)
+    
+    # Save the magnitude spectrum as an image
+    cv2.imwrite(output_path, magnitude_spectrum)
 
-def correlation_coefficient(first: np.ndarray, second: np.ndarray, m: int) -> float:
+def compare_magnitude_spectrums(image_path1, image_path2):
     """
-    Calculate correlation coefficient for a pair of arrays.
-    :param first: first list of means
-    :param second: second list of means
-    :param m: number of means
-    :return: calculated correlation coefficient
+    Compare the magnitude spectrums of two images
+    :param image_path1: path to the first image file
+    :param image_path2: path to the second image file
+    :return: similarity score
     """
-    # Normalisation; mean and standard deviation of normalised data is respectively 0 and 1
-    # (in our case it is approximately 0 and 1)
-    mean_f = np.mean(first)
-    std_f = np.std(first)
-    if std_f:
-        normalised_f = (first - mean_f)/std_f
-    else:
-        normalised_f = np.zeros_like(first)
-    mean_s = np.mean(second)
-    std_s = np.std(second)
-    if std_s:
-        normalised_s = (second - mean_s)/std_s
-    else:
-        normalised_s = np.zeros_like(second)
+    def get_magnitude_spectrum(image_path):
+        ret, image = cv2.VideoCapture(image_path).read()
+        if not ret:
+            raise AssertionError("Error with loading image.")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        f_transform = np.fft.fft2(image)
+        f_transform_shifted = np.fft.fftshift(f_transform)
+        magnitude_spectrum = 20 * np.log(np.abs(f_transform_shifted))
+        magnitude_spectrum = cv2.normalize(magnitude_spectrum, None, 0, 255, cv2.NORM_MINMAX)
+        return magnitude_spectrum
 
-    return float(sum([i*j for i, j in zip(normalised_f, normalised_s)])/m)
+    magnitude_spectrum1 = get_magnitude_spectrum(image_path1)
+    magnitude_spectrum2 = get_magnitude_spectrum(image_path2)
+    
+    # Calculate similarity (e.g., using correlation)
+    similarity = np.corrcoef(magnitude_spectrum1.flatten(), magnitude_spectrum2.flatten())[0, 1]
+    return similarity
 
+
+
+def compare_images_hog(image_path1, image_path2):
+    """
+    Compare two images using HOG
+    :param image_path1: path to the first image file
+    :param image_path2: path to the second image file
+    :return: similarity score
+    """
+    def get_hog_features(image_path):
+        ret, image = cv2.VideoCapture(image_path).read()
+        if not ret:
+            raise AssertionError("Error with loading image.")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16),
+                            cells_per_block=(1, 1), visualize=True)
+        return fd
+
+    hog_features1 = get_hog_features(image_path1)
+    hog_features2 = get_hog_features(image_path2)
+    
+    # Calculate similarity using correlation
+    similarity = np.corrcoef(hog_features1, hog_features2)[0, 1]
+    return similarity
 
 if __name__ == '__main__':
     DATA_DIR = 'data'
-    NUMBER_OF_SAMPLES = 100
-    NUMBER_OF_MEANS = 50
-    CRDS = [generate_random_coordinates(*(243, 320), NUMBER_OF_SAMPLES) for _ in range(NUMBER_OF_MEANS)]
-    generate_means(DATA_DIR, CRDS)
-    data = np.loadtxt('data.csv', delimiter=';')
+    OUTPUT_DIR = 'magnitude_spec'
     labels = [name for name in os.listdir(DATA_DIR) if name.endswith('.gif')]
+    image_shape = (243, 320)
+    
+    # Ensure the output directory exists
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+    
+    for label in labels:
+        image_path = os.path.join(DATA_DIR, label)
+        output_path = os.path.join(OUTPUT_DIR, f'magnitude_spectrum_{label}.png')
+        save_magnitude_spectrum(image_path, output_path)
+    
+    # Compare each image with every other image and save results to CSV using HOG
+    with open('similarity_scores_hog.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Image1', 'Image2', 'Similarity'])
+        
+        for i in range(len(labels)):
+            for j in range(i + 1, len(labels)):
+                image_path1 = os.path.join(DATA_DIR, labels[i])
+                image_path2 = os.path.join(DATA_DIR, labels[j])
+                similarity_score = compare_images_hog(image_path1, image_path2)
+                csv_writer.writerow([labels[i], labels[j], similarity_score])
 
-    # for each pair of images we create a list with ['Pair Name', 'Similarity Score (rho)', 'Ground Truth Match']
-    pairs = [[f"{img1}-{img2}",
-              correlation_coefficient(data[labels.index(img1)], data[labels.index(img2)], NUMBER_OF_MEANS),
-              int(img1[:9] == img2[:9])]
-             for img1 in labels for img2 in labels[labels.index(img1):] if img1 != img2]
 
-    with open('pairs_comparison.csv', 'w', newline='') as file:
-        csv.writer(file, delimiter=';').writerows(pairs)
-
-    # Plotting Receiver Operating Characteristic (ROC) Curve and calculating Area Under Curve (AUC)
-    # False Positive Rate (FPR) = False Positives/(False Positives + True Negatives) = Incorrect Matches/Total Non-Matches
-    # True Positive Rate (TPR)  = True Positives/(True Positives + False Negatives) = Correct Matches/Total Matches
-    # AUC = 1 - perfect classifier; AUC = 0.5 - random classifier (randomly guessing is as good as this); AUC < 0.5 - model is worse than random
-
-    fpr, tpr, thresholds = roc_curve([pair[2] for pair in pairs], [pair[1] for pair in pairs])
-    auc = roc_auc_score([pair[2] for pair in pairs], [pair[1] for pair in pairs])
-
-    plt.plot(fpr, tpr, label=f"AUC = {auc:.2f}")
-    plt.plot([0, 1], [0, 1], 'r--', label="Random Guessing")
-    plt.scatter(x=0.05, y=tpr[np.argmin(np.abs(fpr - 0.05))], color='green',
-                label=f"Point (0.05, {tpr[np.argmin(np.abs(fpr - 0.05))]:.2f})\nThreshold = {thresholds[np.argmin(np.abs(fpr - 0.05))]:.3f}")
-    plt.xlabel('False Positive Rate (FPR)')
-    plt.ylabel('True Positive Rate (TPR)')
-    plt.title(f'ROC Curve for Face Recognition')
-    plt.legend()
-    plt.savefig('ROC_curve.png')
-    plt.close()
-
-    same = [pair[1] for pair in pairs if pair[2] == 1]
-    different = [pair[1] for pair in pairs if pair[2] == 0]
-
-    t_stat, p_value = stats.mannwhitneyu(same, different, alternative='greater', method='asymptotic')
-
-    print(f"Mann-Whitney Test; z: {t_stat}, p-value: {p_value}")
-
-    if p_value < 0.05:
-        print("Rejecting H0 - there is significant difference between the groups. -> H1")
-    else:
-        print("No reason to reject H0 - no significant difference between the groups. -> H0")
-
-    s1, s2 = stats.t.interval(0.95, df=len(same) - 1, loc=np.mean(same), scale=np.std(same, ddof=1) / np.sqrt(len(same)))
-    d1, d2 = stats.t.interval(0.95, df=len(different) - 1, loc=np.mean(different), scale=np.std(different, ddof=1) / np.sqrt(len(different)))
-    print(f"\nConfidence intervals:\nsame-person-pairs: {(float(s1), float(s2))}, mean: {np.mean(same)}\n"
-          f"different-person-pairs: {(float(d1), float(d2))}, mean: {np.mean(different)}")
-
-    plt.figure(figsize=(9,8))
-    plt.hist(same, bins=20, alpha=0.5, color='blue', label='Same-person pairs')
-    plt.hist(different, bins=20, alpha=0.5, color='red', label='Different-person pairs')
-    plt.xticks([i/10 for i in range(-10, 11)])
-    plt.yticks([i for i in range(0, 2000, 100)])
-    plt.grid(True)
-
-    plt.xlabel('Similarity Score (Correlation Coefficient)')
-    plt.ylabel('Frequency')
-    plt.title('Histogram of Similarity Scores - frequency')
-    plt.legend(loc='upper left')
-    plt.savefig('histograms.png')
-    plt.close()
-
-    plt.figure(figsize=(9,8))
-
-    same_counts, same_bins = np.histogram(same, bins=20)
-    same_heights = same_counts / same_counts.sum() * 100
-    diff_counts, diff_bins = np.histogram(different, bins=20)
-    diff_heights = diff_counts / diff_counts.sum() * 100
-    plt.bar(same_bins[:-1], same_heights, width=np.diff(same_bins), alpha=0.5, color='blue', label='Same-person pairs', align='edge')
-    plt.bar(diff_bins[:-1], diff_heights, width=np.diff(diff_bins), alpha=0.5, color='red', label='Different-person pairs', align='edge')
-    plt.yticks([i for i in range(0, 31, 2)], [f"{i}%" for i in range(0, 31, 2)])
-    plt.grid(True)
-
-    plt.xlabel('Similarity Score (Correlation Coefficient)')
-    plt.ylabel('Percentage')
-    plt.title('Histogram of Similarity Scores - percentage')
-    plt.legend(loc='upper left')
-    plt.savefig('histograms_percentage.png')
-    plt.close()
